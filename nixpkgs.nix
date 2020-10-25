7e6183a51fe3cc92c4d27d3ab1a046ad45fa5835
meck <Johan@meck.se>
2020-10-25 20:53:24 +0100

    update nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,8 +1,8 @@
 let
   nixpkgs = builtins.fetchTarball {
-    name = "nixos-unstable-2020-09-08";
+    name = "nixos-unstable-2020-10-25";
     url =
-      "https://github.com/nixos/nixpkgs/archive/5aba0fe9766a7201a336249fd6cb76e0d7ba2faf.tar.gz";
-    sha256 = "05gawlhizp85agdpw3kpjn41vggdiywbabsbmk76r2dr513188jz";
+      "https://github.com/nixos/nixpkgs/archive/bd017760509492a1774b0ea2fc5c1bbb4b2d98af.tar.gz";
+    sha256 = "0rd7dc677s76vham7mjfc7286lfigi0ynx32ls14ffi7xwy9js42";
   };
 in (import nixpkgs) { overlays = [ (import ./overlay.nix) ]; }
